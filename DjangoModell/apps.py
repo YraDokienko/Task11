bc8064dd62ca9d889335a10569bee8932296a5a9
YraDokienko <uradokienko@gmail.com>
2019-11-02 17:26:35 +0200

    commit

---
+++ DjangoModell/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangomodellConfig(AppConfig):
+    name = 'DjangoModell'
---
+++ DjangoModell/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class AllModels(models.Model):
+    Auto =
+    BigInteger =
+    Binary =
+    Boolean =
+    Char =
+    CommaSeparatedInteger =
+    Date =
+    DateTime =
+    Duration =
+    Email =
+    File =
+    FilePath =
+    Float =
+    Image =
+    Integer =
+    GenericIPAddress =
+    NullBoolean =
+    PositiveInteger =
+    PositiveSmallInteger=
+    Slug =
+    SmallInteger =
+    Text =
+    Time =
+    URL =
+    UUID =
